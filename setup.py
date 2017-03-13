c3ffa68f76b21978ce91589912c93d7d8a3840d0
Pascal Corpet <pascal+github@corpet.net>
2017-03-13 17:56:40 +0100

    Release v0.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """Python client library for Pôle Emploi's "Emploi Store Dev"."""
 import setuptools
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 setuptools.setup(
     name='python-emploi-store',
